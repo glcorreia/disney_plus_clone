5676f9a6987eed11d4b893306bac77c48d5d2b27
Goncalo Correia <gonkbsd@gmail.com>
2022-05-15 22:35:08 +0100

    starting app

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+	<Container>
+		header
+	</Container>
+  )
+}
+
+export default Header
+
+const Container = styled.div`
+	height: 70px;
+	background-color: #090b13;
+`
